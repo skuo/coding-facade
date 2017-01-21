29101829be8226d633c30f76349193c42a8b793b
Steve Kuo <steve.kuo@makerstudios.com>
2017-01-20 16:25:43 -0800

    Use @Value for codingServiceUsername and Password

--- src/main/java/com/coding/facade/rest/client/CodingFeignClientConfig.java
+++ src/main/java/com/coding/facade/rest/client/CodingFeignClientConfig.java
@@ -1,5 +1,8 @@
 package com.coding.facade.rest.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,8 +10,18 @@ import feign.auth.BasicAuthRequestInterceptor;
 
 @Configuration
 public class CodingFeignClientConfig{
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${coding.service.username}")
+    String codingServiceUsername;
+    
+    @Value("${coding.service.password}")
+    String codingServicePassword;
+
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
-         return new BasicAuthRequestInterceptor("user", "CodingBreak");
+        log.info(String.format("<<<< In CodingFeignClientConfig codingServiceUsername=%s, codingServicePassword=%s",
+                codingServiceUsername, codingServicePassword));
+        return new BasicAuthRequestInterceptor(codingServiceUsername, codingServicePassword);
     }
 }
